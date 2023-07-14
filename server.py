9dbc9d546cb8aedbdeaa38ca7ddb61cef8115498
Maziar Sojoudian <maziar@Maziars-MacBook-Air.local>
2023-07-14 16:47:18 -0400

    create web server

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+import os
+
+PORT = 5555
+
+DIRECTORY = "./templates/"
+
+os.chdir(DIRECTORY)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Server is started on {PORT} from directory {DIRECTORY}")
+    httpd.serve_forever()
